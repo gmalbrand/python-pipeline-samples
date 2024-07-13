6ee8b7ccf3ee55570e422121d001286a865cc131
Guillaume Malbrand <gmalbrand@free.fr>
2024-07-13 16:13:36 +0200

    Just a simple commit

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask , render_template
-app =   Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+# Just add a comment
+
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True , host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
